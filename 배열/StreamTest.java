9033c9bc671b4fa6d0d845f67b2c0203b5143ef7
hykim02 <hykim4435@naver.com>
2025-06-13 14:57:24 +0900

    practice

--- 배열/StreamTest.java
+++ 배열/StreamTest.java
@@ -3,10 +3,17 @@ import java.util.stream.Stream;
 
 public class StreamTest {
     public static void main(String[] args) {
-        // 스트림 생성성
+        // 스트림 생성
         String[] arr = new String[]{"a", "b", "c"};
         Stream<String> stream = Arrays.stream(arr);
 
         stream.forEach(System.out::println);
+
+        String str = "ABC";
+        String l = str.toLowerCase();
+        System.out.println(str);
+        System.out.println(l);
+        System.out.println(str.substring(1));
+        System.out.println(str.substring(0, 2));
     }
 }
