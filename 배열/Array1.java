b5026f426b0418ef736beefb4796e44483c17d7b
hykim02 <hykim4435@naver.com>
2025-06-15 23:36:28 +0900

    배열 실습

---
+++ 배열/Array1.java
@@ -0,0 +1,16 @@
+public class Array1 {
+
+    public static void main(String[] args) {
+        int s1 = 90;
+        int s2 = 80;
+        int s3 = 70;
+        int s4 = 60;
+        int s5 = 50;
+
+        System.out.println("학생1 점수: " + s1);
+        System.out.println("학생2 점수: " + s2);
+        System.out.println("학생3 점수: " + s3);
+        System.out.println("학생4 점수: " + s4);
+        System.out.println("학생5 점수: " + s5);
+    }
+}
